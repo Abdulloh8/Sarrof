6e8769c365aab315a8375b25692a2c0b7ed9c142
Your Name <you@example.com>
2023-07-19 22:07:31 +0500

    end

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, do you want to exchange dollars for sums?");
+Console.Write("how many dollars : ");
+decimal dollars = Convert.ToDecimal(Console.ReadLine());
+decimal sum = dollars * 11600;
+Console.WriteLine($"you can get {sum} So'm");
