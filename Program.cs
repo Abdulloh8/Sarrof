9eac3446fcb1b38e7080722443a43b665cf660a4
Your Name <you@example.com>
2023-07-22 12:23:04 +0500

    Main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿Console.WriteLine("Hello, do you want to exchange dollars for sums?");
-Console.Write("how many dollars : ");
-decimal dollars = Convert.ToDecimal(Console.ReadLine());
-decimal sum = dollars * 11600;
-Console.WriteLine($"you can get {sum} So'm");
+﻿namespace Sarrof
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, do you want to exchange dollars for sums?");
+            Console.Write("how many dollars : ");
+            decimal dollars = Convert.ToDecimal(Console.ReadLine());
+            decimal sum = dollars * 11600;
+            Console.WriteLine($"you can get {sum} So'm");
+        }
+    }
+}
+
