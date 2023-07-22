1ec48711f972df0b65618e6faf249d59cb998ffb
Your Name <you@example.com>
2023-07-22 13:09:04 +0500

    Add New Version

--- Program.cs
+++ Program.cs
@@ -4,11 +4,37 @@
     {
         static void Main()
         {
-            Console.WriteLine("Hello, do you want to exchange dollars for sums?");
-            Console.Write("how many dollars : ");
-            decimal dollars = Convert.ToDecimal(Console.ReadLine());
-            decimal sum = dollars * 11600;
-            Console.WriteLine($"you can get {sum} So'm");
+            decimal sum = default;
+            decimal dollars = default;
+
+            Console.WriteLine("Hello, do you want to exchange many");
+            Console.WriteLine("1. from Dollar to so'm : ");
+            Console.WriteLine("2. from So'm to dollar : ");
+            string message = Console.ReadLine();
+            if(message == "1")
+            {
+                Console.Write("how many dollars : ");
+                dollars = Convert.ToDecimal(Console.ReadLine());
+                sum = dollars * 11600;
+            }
+            else if(message == "2")
+            {
+                Console.Write("how many So'm : ");
+                sum = Convert.ToDecimal(Console.ReadLine());
+                dollars = sum / 11600;
+            }
+            else
+            {
+                Console.WriteLine("Error message");
+            }
+
+            string finsh = (message == "1")
+                         ? $"you can get {sum} So'm"
+                         : (message == "2")
+                         ? $"you can get {dollars} dollars"
+                         : "Error message";
+
+            Console.WriteLine(finsh);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Sarrof
-money exchange place
+this project implements money exchange
+
+Have a look at *`Program.cs`* file and you can see hoe I have used: 
+
+* Console input/output
+* I use if operation
+
+
+## Demo 
+
+![Demo](./Снимок%20экрана%202023-07-22%20130202.png)
