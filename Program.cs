8b4af7877c7668d6ffb348b956e146408d10ef17
Your Name <you@example.com>
2023-07-25 12:30:15 +0500

    Switch Operation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Sarrof
+﻿
+namespace Sarrof
 {
     class Program
     {
@@ -11,28 +12,32 @@
             Console.WriteLine("1. from Dollar to so'm : ");
             Console.WriteLine("2. from So'm to dollar : ");
             string message = Console.ReadLine();
-            if(message == "1")
-            {
-                Console.Write("how many dollars : ");
-                dollars = Convert.ToDecimal(Console.ReadLine());
-                sum = dollars * 11600;
-            }
-            else if(message == "2")
-            {
-                Console.Write("how many So'm : ");
-                sum = Convert.ToDecimal(Console.ReadLine());
-                dollars = sum / 11600;
-            }
-            else
-            {
-                Console.WriteLine("Error message");
-            }
 
-            string finsh = (message == "1")
-                         ? $"you can get {sum} So'm"
-                         : (message == "2")
-                         ? $"you can get {dollars} dollars"
-                         : "Error message";
+
+            switch(message)
+            {
+                case "1": Console.Write("how many dollars : ");
+                          dollars = Convert.ToDecimal(Console.ReadLine());
+                          sum = dollars * 11600;
+                       break;
+                case "2": Console.Write("how many So'm : ");
+                          sum = Convert.ToDecimal(Console.ReadLine());
+                          dollars = sum / 11600;
+                       break;
+                default : Console.WriteLine("Error message");
+                       break;          
+            }
+            
+            string finsh;
+            switch (message)
+            {
+                case "1": finsh = $"you can get {sum} So'm";
+                    break;
+                case "2": finsh = $"you can get {dollars} dollars";
+                    break;  
+                default : finsh = "Error message"; 
+                    break;   
+            };
 
             Console.WriteLine(finsh);
         }
